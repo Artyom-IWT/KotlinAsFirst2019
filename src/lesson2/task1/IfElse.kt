5a4f4621adfb2af4d5ce93b4e417256f8195f21c
Artyom <untila_artem@mail.ru>
2019-10-02 19:51:39 +0300

    KotlinSecondLesson FINALLY CORRECTED

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -91,10 +91,10 @@ fun timeForHalfWay(
     val s1 = t1 * v1
     val s2 = t2 * v2
     val s3 = t3 * v3
-    val s4 = ( s1 + s2 + s3 ) / 2
-    if (s1 >= s4) return s4 / v1
-    else if ((s1 + s2) >= s4) return t1 + ((s4 - s1) / v2)
-    else return t1 + t2 + ((s4 - s1 - s2) / v3)
+    val s4 = (s1 + s2 + s3) / 2
+    return if (s1 >= s4)  s4 / v1
+    else if ((s1 + s2) >= s4)   t1 + ((s4 - s1) / v2)
+    else t1 + t2 + ((s4 - s1 - s2) / v3)
 }
 
 /**
